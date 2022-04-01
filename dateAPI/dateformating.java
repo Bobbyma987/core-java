8acff4428c9ba3eff061f198d22e38018b8c48af
Bobbyma987 <b.marwha@zensar.com>
2022-04-01 12:12:52 +0530

    date formating

---
+++ dateAPI/dateformating.java
@@ -0,0 +1,11 @@
+package dateAPI;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class dateformating {
+    public static void main(String[] args) {
+           // SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/mm/yy");
+           LocalDate ld = LocalDate.now();
+           DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MMMM/uuuu");
+           System.out.println(ld.format(df)); 
+    }
+}
